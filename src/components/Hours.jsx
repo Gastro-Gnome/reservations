8dd1f595ea3fc4272c47e8ab60478c251eb3db48
Jackson Galan <jacksongalan@gmail.com>
2019-07-09 16:24:44 -0700

    add value attribute to Hours component

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+//call the server for biz hours
+//pass a bizhoursItem from the database
+//to hours component
+//th attr name is var to render content
 
 const Hours = () => {
     return(
@@ -6,13 +10,16 @@ const Hours = () => {
             <h3>Hours</h3>
             <table className="table-hours">
                 <tbody>
-                    <tr><th scope="row">M</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
-                    <tr><th scope="row">T</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
-                    <tr><th scope="row">W</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
-                    <tr><th scope="row">R</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Mon">Mon</th><td><span value="0900">TODO</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Tue">Tue</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Wed">Wed</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Thu">Thu</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Fri">Fri</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Sat">Sat</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th scope="row"name="Sun">Sun</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
                 </tbody>
             </table>
-        </div>
+        </div> 
     )
 }
 export default Hours;
