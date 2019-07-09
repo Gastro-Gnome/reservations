6b9de3bcaba3b0aac72b9b9ccea26ad1d33519cb
Jackson Galan <jacksongalan@gmail.com>
2019-07-09 14:32:46 -0700

    corrected typos in fs and routes

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,13 @@
 //generate an array of objects to pass to the model for document creation
 const db = require("./index.js");
 const Model = require("./model.js");
-const sampleData = require("./generateData.js");
+const mockData = require("./mockData.js");
 
 const seed = function() {
     //TODO
     console.log("seed script initiated");
    //an array of gizhoursItems
-   Model.insertMany(sampleData, (err, docs) => {
+   Model.insertMany(mockData, (err, docs) => {
        if (err) {
            console.error(err);
        } else {
