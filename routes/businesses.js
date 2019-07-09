1ad61be43e0b9c35fd8237b0c2f0afea97513faa
Jackson Galan <jacksongalan@gmail.com>
2019-07-09 14:18:18 -0700

    add businesses route

--- routes/businesses.js
+++ routes/businesses.js
@@ -2,31 +2,21 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/index.js");
 const Model = require("../db/model.js");
+const seed = require("../db/seed.js");
 
-let someBizHours = {
-    biz_id: 23,
-    hoursByDay: {
-        Mon: {
-            open_at: 8,
-            close_at: 21
-        }
-    }
-}
-
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
     //TODO -- query the db for hoursItems bizId 1
-    console.log("/businesses received a GET request");
-    //instantiate doc
-    let someBizHoursDoc = new Model(someBizHours);
-    someBizHoursDoc.save((error) => {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log("Saved!");
-        }
-    })
+    console.log("/businesses < GET request");
+    let query = await Model.find();
 
-  res.end();
+  res.send(query);
 });
+
+router.post('/', (req, res, next) => {
+    console.log("/businesses < POST request");
+    //TODO -- seed db from script
+    seed();
+    res.end()
+})
 
 module.exports = router;
