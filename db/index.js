8d92a9abaa595248c2fc601d26caa54e4d8ec6a9
Jackson Galan <jacksongalan@gmail.com>
2019-07-09 14:17:12 -0700

    expanded schema to full week

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Model = require("./model.js");
 
-mongoose.connect("mongodb://127.0.0.1/reservations", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1/test", {useNewUrlParser: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- db/model.js
+++ db/model.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 const hoursSchema = require("./schema.js");
 
-const HoursItem = mongoose.model("hours_item", hoursSchema);
+const HoursItem = mongoose.model("HoursItem", hoursSchema);
+
+//TEST
+
+let someBizHours = {
+    biz_id: 23,
+    hoursByDay: {
+        Mon: {
+            open_at: 8,
+            close_at: 21
+        }
+    }
+}
 
 module.exports = HoursItem;
