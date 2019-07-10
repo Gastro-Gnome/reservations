f717f3cb638a6d06926d32b518f42c4030f8b122
Jackson Galan <jacksongalan@gmail.com>
2019-07-10 11:56:52 -0700

    setup a simple test using jext and enzyme

--- __test__/shallowRender.test.js
+++ __test__/shallowRender.test.js
@@ -2,11 +2,14 @@
 // test file
 import { shallow, mount, render } from 'enzyme';
 import React from "react";
-import App from "../src/components/App"
+import App from "../src/components/App";
+import toJSON from "enzyme-to-json";
+import { shouldInstrument } from 'jest-runtime';
+import { isTSAnyKeyword } from '@babel/types';
 
-test("shallow wrapper instance should not be null", () => {
-    const wrapper = shallow(<App />);
-    const instance = wrapper.instance();
-
-    expect(instance).to.be.instanceOf(App);
-})
+describe('App', () => {
+    it('renders without crashing', () => {
+      const wrapper = shallow(<App />);
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+  });
