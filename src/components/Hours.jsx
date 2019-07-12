b5957e5e55f3240a0350618299f629844a4470c5
Jackson Galan <jacksongalan@gmail.com>
2019-07-12 10:46:32 -0700

    db refactor  to sync mock and db data in progress

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -3,13 +3,14 @@ import styles from "../main.css";
 //Pass props to Hours
 //if the api call is not done, do not render
 
-const Hours = () => {
+const Hours = (props) => {
+    console.log(props.hoursItem);
     return(
         <div className="service business-hours">
             <h3>Hours</h3>
             <table className="table-hours">
                 <tbody>
-                    <tr><th>Mon</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra">Open Now</td></tr>
+                    <tr><th>Mon</th><td><span>{props.hoursItem.Mon.open_at}</span> - <span>5:00pm</span></td><td className="extra">Open Now</td></tr>
                     <tr><th>Tue</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
                     <tr><th>Wed</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
                     <tr><th>Thu</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
