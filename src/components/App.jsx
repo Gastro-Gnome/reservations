57d299ef3d2208288f2a9f62a06d95cdea31979e
Jackson Galan <jacksongalan@gmail.com>
2019-07-12 11:19:16 -0700

    Hours component renders from API call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from "react";
 import axios from "axios";
 import Hours from "./Hours.jsx";
-//pass mock data to Hours, after componentDidMount
-//pass db data to Hours
 
 class App extends Component {
     constructor(props) {
