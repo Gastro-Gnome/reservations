8965266662630c5e557a47ef176b5dbfbba9d302
Jackson Galan <jacksongalan@gmail.com>
2019-07-12 10:53:06 -0700

    pared down schema to days of the week

--- db/schema.js
+++ db/schema.js
@@ -1,43 +1,34 @@
 const mongoose = require("mongoose");
 
 const hoursSchema = new mongoose.Schema({
-    biz_id: Number,
-    hoursByDay: {
-      Mon: {
-          open_at: String,
-          close_at: String
-      },
-      Tue: {
-          open_at: String,
-          close_at: String
-      },
-      Wed: {
-          open_at: String,
-          close_at: String
-      },
-      Thu: {
-          open_at: String,
-          close_at: String
-      },
-      Fri: {
-          open_at: String,
-          close_at: String
-      },
-      Sat: {
-          open_at: String,
-          close_at: String
-      },
-      Sun: {
-          open_at: String,
-          close_at: String
-      }
-  }
+    Mon: {
+        open_at: String,
+        close_at: String
+    },
+    Tue: {
+        open_at: String,
+        close_at: String
+    },
+    Wed: {
+        open_at: String,
+        close_at: String
+    },
+    Thu: {
+        open_at: String,
+        close_at: String
+    },
+    Fri: {
+        open_at: String,
+        close_at: String
+    },
+    Sat: {
+        open_at: String,
+        close_at: String
+    },
+    Sun: {
+        open_at: String,
+        close_at: String
+    }
  });
 
-hoursSchema.methods.findBizById = async function(number) {
-   let query = await HoursItem.find({
-       biz_id: number
-   })
-   return query;
-}
 module.exports = hoursSchema;
