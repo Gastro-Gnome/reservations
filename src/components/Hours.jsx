552b4202b3dae0bc9fa73fca9ba9efaf73de2a08
Jackson Galan <jacksongalan@gmail.com>
2019-07-12 10:32:21 -0700

    removed duplicate td els

--- src/components/Hours.jsx
+++ src/components/Hours.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../main.css";
+//Pass props to Hours
+//if the api call is not done, do not render
 
 const Hours = () => {
     return(
@@ -7,13 +9,13 @@ const Hours = () => {
             <h3>Hours</h3>
             <table className="table-hours">
                 <tbody>
-                    <tr><th>Mon</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra">Open Now</td></tr>
-                    <tr><th>Tue</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
-                    <tr><th>Wed</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
-                    <tr><th>Thu</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
-                    <tr><th>Fri</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
-                    <tr><th>Sat</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
-                    <tr><th>Sun</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td><td className="extra"></td></tr>
+                    <tr><th>Mon</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra">Open Now</td></tr>
+                    <tr><th>Tue</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th>Wed</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th>Thu</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th>Fri</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th>Sat</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
+                    <tr><th>Sun</th><td><span>9:00am</span> - <span>5:00pm</span></td><td className="extra"></td></tr>
                 </tbody>
             </table>
         </div> 
