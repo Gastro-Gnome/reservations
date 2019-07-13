d6c94901432a4c8a91d0bb926640219d8ad2f686
Jackson Galan <jacksongalan@gmail.com>
2019-07-12 19:13:27 -0700

    add input for date and placehodler value

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -11,6 +11,10 @@ let Reservations = () => {
                 <form className="reservation-availability-search-form">
                     <ul className="inline-layout reservation-fields">
                         <li className="date-picker"></li>
+                        <div className="select">
+                         <span><svg id="18x18_clock" height="18" viewBox="0 0 18 18" width="18"><path d="M9 17A8 8 0 1 1 9 1a8 8 0 0 1 0 16zM9 2C5.14 2 2 5.14 2 9s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm3.223 10.098a.998.998 0 0 1-.588-.192L8 9.256V5a1 1 0 0 1 2 0v3.24l2.812 2.05a1 1 0 0 1-.59 1.808z"></path></svg></span>
+                         <input class="date-input js-date-input reservation-input" name="reservation_datetime_date" type="text" value="TEST" readonly=""></input>
+                        </div>
 
                     </ul>
                 </form>
@@ -23,9 +27,7 @@ export default Reservations;
 
 //ICONS
 
-{/* <span>
-<svg id="18x18_clock" height="18" viewBox="0 0 18 18" width="18"><path d="M9 17A8 8 0 1 1 9 1a8 8 0 0 1 0 16zM9 2C5.14 2 2 5.14 2 9s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm3.223 10.098a.998.998 0 0 1-.588-.192L8 9.256V5a1 1 0 0 1 2 0v3.24l2.812 2.05a1 1 0 0 1-.59 1.808z"></path></svg>
-</span>
+{/* 
 <span>
 <svg id="18x18_friends" height="18" viewBox="0 0 18 18" width="18"><g><path d="M7.904 9.43l-2.098 4.697a.9.9 0 0 1-1.612 0L2.096 9.43a.902.902 0 0 1 .806-1.305h4.196c.67 0 1.105.705.806 1.305zM5 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"></path><path d="M15.904 9.43l-2.098 4.697a.89.89 0 0 1-.806.498.89.89 0 0 1-.806-.498L10.096 9.43a.902.902 0 0 1 .806-1.305h4.195c.67 0 1.106.705.807 1.305zM13 7.375a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" opacity=".502"></path></g></svg>
 </span>  */}
