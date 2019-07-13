f3baaba910fed5e26af1be5d2710f8aaf6718115
Jackson Galan <jacksongalan@gmail.com>
2019-07-13 12:14:50 -0700

    update mock data to comply with schema

--- db/mockData.js
+++ db/mockData.js
@@ -1,97 +1,124 @@
 //hardcoded models
 
 let biz1 = {
-    Mon: {
-        open_at: "7:00 am",
-        close_at: "4:30 pm"
-    },
-    Tue: {
-        open_at: "7:00 am",
-        close_at: "4:30 pm"
-    },
-    Wed: {
-        open_at: "7:00 am",
-        close_at: "4:30 pm"
-    },
-    Thu: {
-        open_at: "7:00 am",
-        close_at: "4:30 pm"
-    },
-    Fri: {
-        open_at: "7:00 am",
-        close_at: "4:30 pm"
-    },
-    Sat: {
-        open_at: "10:00 am",
-        close_at: "4:30 pm"
-    },
-    Sun: {
-        open_at: "10:00 am",
-        close_at: "4:30 pm" 
-    }
- }
+    biz_hrs: [
+        {0: {
+            is_open: true,
+            open_at: "7:00 am",
+            close_at: "4:30 pm"
+        }},
+        {1: {
+            is_open: true,
+            open_at: "7:00 am",
+            close_at: "4:30 pm"
+        }},
+        {2: {
+            is_open: true,
+            open_at: "7:00 am",
+            close_at: "4:30 pm"
+        }},
+        {3: {
+            is_open: true,
+            open_at: "7:00 am",
+            close_at: "4:30 pm"
+        }},
+        {4: {
+            is_open: true,
+            open_at: "7:00 am",
+            close_at: "4:30 pm"
+        }},
+        {5: {
+            is_open: true,
+            open_at: "10:00 am",
+            close_at: "8:00 pm"
+        }},
+        {6: {
+            is_open: true,
+            open_at: "10:00 am",
+            close_at: "8:00 pm"
+        }}
+    ]
+}
 
  let biz2 = {
-    Mon: {
-        open_at: "8:00 am",
-        close_at: "4:30 pn"
-    },
-    Tue: {
-        open_at: "8:00 am",
-        close_at: "4:30 pn"
-    },
-    Wed: {
-        open_at: "8:00 am",
-        close_at: "4:30 pn"
-    },
-    Thu: {
-        open_at: "8:00 am",
-        close_at: "4:30 pn"
-    },
-    Fri: {
-        open_at: "8:00 am",
-        close_at: "4:30 pn"
-    },
-    Sat: {
-        open_at: "10:00 am",
-        close_at: "7:00 pm"
-    },
-    Sun: {
-        open_at: "10:00 am",
-        close_at: "7:00 pm"
-    }
- }
+    biz_hrs: [
+        {0: {
+            is_open: true,
+            open_at: "8:00 am",
+            close_at: "5:00 pm"
+        }},
+        {1: {
+            is_open: true,
+            open_at: "8:00 am",
+            close_at: "5:00 pm"
+        }},
+        {2: {
+            is_open: true,
+            open_at: "8:00 am",
+            close_at: "5:00 pm"
+        }},
+        {3: {
+            is_open: true,
+            open_at: "8:00 am",
+            close_at: "5:00 pm"
+        }},
+        {4: {
+            is_open: true,
+            open_at: "8:00 am",
+            close_at: "5:00 pm"
+        }},
+        {5: {
+            is_open: true,
+            open_at: "12:00 pm",
+            close_at: "10:00 pm"
+        }},
+        {6: {
+            is_open: true,
+            open_at: "12:00 pm",
+            close_at: "10:00 pm"
+        }}
+    ]
+}
 
  let biz3 = {
-    Mon: {
-        open_at: "5:00 pm",
-        close_at: "12:00 am"
-    },
-    Tue: {
-        open_at: "5:00 pm",
-        close_at: "12:00 am"
-    },
-    Wed: {
-        open_at: "5:00 pm",
-        close_at: "12:00 am"
-    },
-    Thu: {
-        open_at: "5:00 pm",
-        close_at: "12:00 am"
-    },
-    Fri: {
-        open_at: "5:00 pm",
-        close_at: "12:00 am"
-    },
-    Sat: {
-        open_at: "5:00 pm",
-        close_at: "2:00 am"
-    },
-    Sun: {
-        open_at: "5:00 pm",
-        close_at: "2:00 am"
-    }
- }
+    biz_hrs: [
+        {0: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {1: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {2: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {3: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {4: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {5: {
+            is_open: true,
+            open_at: "4:30 pm",
+            close_at: "12:00 am"
+        }},
+        {6: {
+            is_open: false,
+            open_at: "",
+            close_at: ""
+        }}
+    ]
+}
 
  let sampleBizHoursItems = [biz1, biz2, biz3];
 
