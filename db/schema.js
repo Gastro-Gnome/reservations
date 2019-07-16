52fd445a6c915a8499419d3642cad5270511eabe
Jackson Galan <jacksongalan@gmail.com>
2019-07-15 19:33:38 -0700

    refactored schema to an array of day objects

--- db/schema.js
+++ db/schema.js
@@ -1,42 +1,50 @@
 const mongoose = require("mongoose");
 
 const hoursSchema = new mongoose.Schema({
-    biz_hrs: [
-        {0: {
-            is_open: Boolean,
+    biz_id: Number,
+    days: [
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {1: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {2: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {3: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {4: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {5: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }},
-        {6: {
-            is_open: Boolean,
+        },
+        {
+            day: Number,
+            open: Boolean,
             open_at: String,
             close_at: String
-        }}
+        }
     ]
 })
 
