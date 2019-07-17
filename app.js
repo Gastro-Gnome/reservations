a35a36e0576cbbfb1808472a1910c07e48f2e799
Jackson Galan <jacksongalan@gmail.com>
2019-07-17 11:48:39 -0700

    add port 3004 to serv

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const path = require("path")
 const express = require("express");
 const app = express();
 const businessesRouter = require("./routes/businesses.js");
-const port = 3000;
+const port = 3004;
 
 app.use("/", express.static("./dist"));
 app.use("/businesses", businessesRouter);
 
 
-app.listen(port, console.log(`Apertif Reserv listening on port ${port}!`));
+app.listen(port, console.log(`Reservations listening on port ${port}!`));
 
 module.exports = app;
