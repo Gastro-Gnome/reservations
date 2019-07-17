da749cccf880cd1a458a65c3647bd7938ac411f0
Jackson Galan <jacksongalan@gmail.com>
2019-07-17 10:24:05 -0700

    refactor mockData 'day' to 'dayId'

--- db/mockData.js
+++ db/mockData.js
@@ -4,43 +4,43 @@ let biz1 = {
     biz_id: 1,
     days: [
         {
-            day: 0,
+            dayId: 0,
             open: true,
             open_at: 700,
             close_at: 1700
         },
         {
-            day: 1,
+            dayId: 1,
             open: true,
             open_at: 700,
             close_at: 1700
         },
         {
-            day: 2,
+            dayId: 2,
             open: true,
             open_at: 700,
             close_at: 1700
         },
         {
-            day: 3,
+            dayId: 3,
             open: true,
             open_at: 700,
             close_at: 1700
         },
         {
-            day: 4,
+            dayId: 4,
             open: true,
             open_at: 700,
             close_at: 1700
         },
         {
-            day: 5,
+            dayId: 5,
             open: true,
             open_at: 800,
             close_at: 1700
         },
         {
-            day: 6,
+            dayId: 6,
             open: false,
             open_at: undefined,
             close_at: undefined
@@ -52,43 +52,43 @@ let biz1 = {
     biz_id: 1,
     days: [
         {
-            day: 0,
+            dayId: 0,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 1,
+            dayId: 1,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 2,
+            dayId: 2,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 3,
+            dayId: 3,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 4,
+            dayId: 4,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 5,
+            dayId: 5,
             open: true,
             open_at: 800,
             close_at: 2000
         },
         {
-            day: 6,
+            dayId: 6,
             open: false,
             open_at: undefined,
             close_at: undefined
@@ -100,43 +100,43 @@ let biz1 = {
     biz_id: 1,
     days: [
         {
-            day: 0,
+            dayId: 0,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 1,
+            dayId: 1,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 2,
+            dayId: 2,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 3,
+            dayId: 3,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 4,
+            dayId: 4,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 5,
+            dayId: 5,
             open: true,
             open_at: 1000,
             close_at: 1800
         },
         {
-            day: 6,
+            dayId: 6,
             open: true,
             open_at: 1000,
             close_at: 2000
